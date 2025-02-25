f0585124157a0eb2853f7ed2a41717da09691259
Małgorzata Słota <malgorzataslota@MacBook-Air-Magorzata-2.local>
2025-02-25 15:55:35 +0100

    theme provider fix

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,6 +1,12 @@
+import theme from "./src/assets/styles/theme";
+import { ThemeProvider } from "styled-components"
+
 const React = require('react');
 const { MainTemplate } = require('./src/components/MainTemplate');
 
+
 exports.wrapPageElement = ({ element, props }) => (
-	<MainTemplate {...props}>{element}</MainTemplate>
+	<ThemeProvider theme={theme}>
+		<MainTemplate {...props}>{element}</MainTemplate>
+	</ThemeProvider>
 );
