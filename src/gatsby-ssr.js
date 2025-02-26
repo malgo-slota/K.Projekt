b0f2bd5bb91508aad5d29dc09d82b273a07b422b
Małgorzata Słota <malgorzataslota@MacBook-Air-Magorzata-2.local>
2025-02-26 15:33:29 +0100

    theme deploy issue

--- src/gatsby-ssr.js
+++
@@ -1,7 +0,0 @@
-import React from "react"
-import { ThemeProvider } from "styled-components"
-import theme from "./src/assets/styles/theme";
-
-export const wrapPageElement = ({ element }) => (
-  <ThemeProvider theme={theme}>{element}</ThemeProvider>
-)
